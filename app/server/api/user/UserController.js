792e33450ac0332e195a76d6137aa97fc471d6e8
Thynker <drogoth8@gmail.com>
2021-10-05 10:38:17 +0200

    KeyCloak Logout .

--- app/server/api/user/UserController.js
+++ app/server/api/user/UserController.js
@@ -13,7 +13,7 @@ export class UserController extends BaseController {
 
         this.router.get('/login', App.keycloak.protect("realm:user") ,asyncHandler((res, req, next) => { this.login(res, req, next); }));
         this.router.get('/getUser', asyncHandler((res, req, next) => { this.getSession(res, req, next); }));
-
+        this.router.get('/logout', asyncHandler((res, req, next) => { this.logout(res, req, next); }));
         this.router.get('/anonymous', App.keycloak.protect("realm:protect") , function (req, res) {
             var jsRes = new JsonResponse();
             jsRes.success = false;
@@ -46,6 +46,24 @@ export class UserController extends BaseController {
      * @ {String} Username  Nombre de usuario
      */
 
+    /**
+     * Cierre de sessión
+     *
+     *
+     * @ {get} /logout Request Session information
+     * @ Log out
+     * @ User
+     *
+     *
+     * @ {Boolean} success
+     * @ {Object[]} data  dataObject
+     * @ {String} Username  Nombre de usuario
+     */
+    logout(request, response) {
+        req.session.destroy();
+        res.clearCookie('connect.sid', { path: '/' });
+        res.redirect(App.keycloak.logoutUrl());
+    }
 
 
     updateDashboard(request, response) {
