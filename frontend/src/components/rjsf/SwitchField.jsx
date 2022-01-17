fb722699ae5b3eeeeb0861e15096e8d2dc007e2a
iago sardiña <iago.sardina@landrasistemas.es>
2022-01-17 13:09:22 +0100

    Fix switchNode Type

--- frontend/src/components/rjsf/SwitchField.jsx
+++ frontend/src/components/rjsf/SwitchField.jsx
@@ -1,19 +1,14 @@
 import { Switch } from "antd";
 
 const SwitchField = function (props) {
-    console.log(props);
     return (
-        <div style={{ paddingTop: 5 }}>
-            <label>{props.label}</label>
-            <br />
-            <Switch
-                style={{ marginTop: 5 }}
-                id="custom"
-                className={props.value ? "checked" : "unchecked"}
-                checked={props.value}
-                onChange={(checked) => props.onChange(checked)}
-            />
-        </div>
+        <Switch
+            style={{ marginTop: 5 }}
+            id="custom"
+            className={props.value ? "checked" : "unchecked"}
+            checked={props.value}
+            onChange={(checked) => props.onChange(checked)}
+        />
     );
 };
 
