50047990092042ca994b930a1f479e206174199d
Raúl González <raulzc3pc@gmail.com>
2022-01-17 19:06:16 +0100

    Información de las trazas del mensaje desplegable

--- frontend/src/pages/administration/message/Message.js
+++ frontend/src/pages/administration/message/Message.js
@@ -1,14 +1,17 @@
-import { Modal, Timeline, Spin, Divider } from "antd";
+import { Modal, Timeline, Spin, Divider, Button } from "antd";
+import Icon from "@mdi/react";
 import lodash from "lodash";
 import axios from "axios";
 import moment from "moment";
 import { useEffect, useState } from "react";
 import T from "i18n-react";
+import { mdiTimelinePlus, mdiTimelineMinus } from "@mdi/js";
 
 export default function Message({ visible, onCancel, messageData, integration, channel }) {
     const { breadcrumb_id: messageId, traces } = messageData;
     const [nodes, setNodes] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [displayed, setDisplayed] = useState({});
 
     useEffect(() => {
         getChannelData();
@@ -29,26 +32,110 @@ export default function Message({ visible, onCancel, messageData, integration, c
     const messages = traces.map((item) => item._source);
     const groupedMessages = groupNodes(messages, nodes);
 
-    const timelineItems = groupedMessages.map((item, index) => {
-        const timelineContent = item.data.map((message) => {
-            const date = moment(message.date_time).format("DD/MM/YYYY HH:mm:ss");
+    const timelineItems = groupedMessages.map((item, msgIndex) => {
+        let messageStart = "";
+        let exchange = "";
+        let messageEnd = "";
+        const nodeName = item.node ? item.node : item.error ? "Error" : "Nodo eliminado";
+        const timelineContent = item.data.map((message, index, array) => {
+            const date = moment(message.date_time).format("DD/MM/YYYY HH:mm:ss:SSS");
+            if (index === 0) {
+                messageStart = date;
+                exchange = message.exchange_id;
+            }
+            if (index === array.length - 1) {
+                messageEnd = date;
+            }
+            //TODO: Mostrar el detalle del error (error_stack)(?)
+            const errorContent =
+                message.event === "ERROR" ? (
+                    <>
+                        <p>
+                            <b>Mensaje de error:</b>
+                            {` ${message.error_msg}`}
+                        </p>
+                        {/*  <p>
+                            <b>Excepción:</b>
+                            {` ${message.error_stack}`}
+                        </p> */}
+                    </>
+                ) : null;
+            const successContent =
+                message.event !== "ERROR" ? (
+                    <>
+                        <p>
+                            <b>Endpoint: </b>
+                            {" " + message.node_endpoint}
+                        </p>
+                    </>
+                ) : null;
+
             return (
                 <>
-                    <p>{`${message.arrow ? message.arrow + " | " : ""}${message.event} | ${date}`}</p>
-                    <p>{`${message.exchange_id}`}</p>
-                    <p>{`${message.node_endpoint || ""} `}</p>
-                    <p>{`${message.current_route || ""} `}</p>
+                    <p>
+                        <b>Evento:</b>
+                        {` ${message.event} ${message.arrow ? " | " + message.arrow : ""}`}
+                    </p>
+
+                    <p>
+                        <b>Fecha: </b>
+                        {" " + date}
+                    </p>
+                    <p>
+                        <b>Elapsed: </b>
+                        {" " + message.elapsed}
+                    </p>
+                    <p>
+                        <b>Exchange Id: </b>
+                        {" " + message.exchange_id}
+                    </p>
+                    {successContent}
+                    {errorContent}
                     <Divider></Divider>
                 </>
             );
         });
 
+        const timelineLabel = (
+            <>
+                <p>
+                    <b>{nodeName}</b>
+                </p>
+                {/*  <p>
+                    <b>Excahge Id:</b> {exchange}
+                </p> */}
+                <p>
+                    <b>Inicio:</b> {messageStart}
+                </p>
+                <p>
+                    <b>Fin:</b> {messageEnd}
+                </p>
+                <Divider></Divider>
+            </>
+        );
+
         return (
-            <Timeline.Item
-                key={`message${index}`}
-                label={item.node || "Nodo desconocido"}
-                color={item.error ? "red" : "green"}>
-                {timelineContent}
+            <Timeline.Item key={`message${msgIndex}`} label={timelineLabel} color={item.error ? "red" : "green"}>
+                {displayed["message" + msgIndex] ? (
+                    <>
+                        <p>
+                            <Button
+                                icon={<Icon size="20px" path={mdiTimelineMinus} />}
+                                onClick={() => {
+                                    setDisplayed({ ...displayed, ["message" + msgIndex]: false });
+                                }}></Button>
+                        </p>
+                        {timelineContent}
+                    </>
+                ) : (
+                    <div style={{ height: "7rem" }}>
+                        <Button
+                            icon={<Icon size="20px" path={mdiTimelinePlus} />}
+                            onClick={() => {
+                                setDisplayed({ ...displayed, ["message" + msgIndex]: true });
+                            }}></Button>
+                    </div>
+                )}
             </Timeline.Item>
         );
     });
@@ -66,7 +153,7 @@ export default function Message({ visible, onCancel, messageData, integration, c
                     <Spin tip={T.translate("application.loading")} />
                 </div>
             ) : (
-                <Timeline mode="alternate">{timelineItems}</Timeline>
+                <Timeline mode="left">{timelineItems}</Timeline>
             )}
         </Modal>
     );
