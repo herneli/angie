a965eefbcecb5c64094555883c5063bddd842d72
iago sardiña <iago.sardina@landrasistemas.es>
2022-01-18 15:51:00 +0100

    Reestructurar messagesdao y service

--- app/server/api/messages/index.js
+++ app/server/api/messages/index.js
@@ -1,6 +1,7 @@
-// import { MessageDao } from "./MessageDao";
+import { MessageDao } from "./MessageDao";
 import { MessageService } from "./MessageService";
 import { MessageController } from "./MessageController";
 
 export { MessageService };
+export { MessageDao };
 export { MessageController };
