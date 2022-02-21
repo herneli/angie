1dad30aea3f2b357f7b1ecd7986cd9d0f5ed4fc7
iago sardiña <iago.sardina@landrasistemas.es>
2022-02-21 11:49:07 +0100

    Corregir detalle entidades, no buscaba por codigo.

--- app/server/api/entity/EntityController.js
+++ app/server/api/entity/EntityController.js
@@ -1,7 +1,6 @@
 import { App, BaseController, JsonResponse } from "lisco";
 import expressAsyncHandler from "express-async-handler";
 import { EntityService } from ".";
-import { ConfigurationService } from "../configuration/ConfigurationService";
 
 export class EntityController extends BaseController {
     configure() {
@@ -66,14 +65,7 @@ export class EntityController extends BaseController {
             let service = new EntityService();
             let data = await service.loadById(request.params.id);
 
-            //Tipo entidad
-            let configurationService = new ConfigurationService();
-            let element = await configurationService.list("entity_type",{"id": data.type})
-            if(element.data && element.data[0]){
-                let entity_type = {"entity_type": element.data[0]}
-                data["entity_type_name"] = element &&  element.data[0] && element.data[0].data && element.data[0].data.name ? element.data[0].data.name : ""
-                data = {...data,...entity_type}
-            }
+            
 
             let jsRes = new JsonResponse(true, data);
             let code = 200;
--- app/server/api/entity/EntityService.js
+++ app/server/api/entity/EntityService.js
@@ -1,6 +1,6 @@
 // import { BaseService } from "../../integration/elastic";
 import { EntityDao } from "./EntityDao";
-import { MessageService } from "../messages";
+import { ConfigurationService } from "../configuration/ConfigurationService";
 
 import lodash from "lodash";
 import { CheckpointService } from "../checkpoints";
@@ -11,12 +11,25 @@ export class EntityService extends BaseService {
         super(EntityDao);
     }
 
-    // /**
-    //  * Obtencion de un elemento mediante su identificador
-    //  */
+    /**
+     * Obtencion de un elemento mediante su identificador
+     */
     async loadById(id) {
         const entity = await super.loadById(id);
 
-        return entity.data;
+        let { data } = entity;
+
+        //Tipo entidad
+        let configurationService = new ConfigurationService();
+        let element = await configurationService.list("entity_type", { code: data.type });
+        if (element.data && element.data[0]) {
+            let entity_type = { entity_type: element.data[0] };
+            data["entity_type_name"] =
+                element && element.data[0] && element.data[0].data && element.data[0].data.name
+                    ? element.data[0].data.name
+                    : "";
+            data = { ...data, ...entity_type };
+        }
+        return data;
     }
 }
